98ddb11dfcc6a974bb47457030948f45d45187b2
soulriderx <writerfrenzy81@gmail.com>
2017-12-05 15:41:43 -0800

    added five questions with prompts and if else statements and alerts

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-'usa strict';
+'use strict';
 
-var kids = prompt('how many kids does brian have?');
-console.log('num of kids:', kids);
+var gameOne = prompt('do you play cardfight vanguard? Y or N only.');
+console.log('gameOne:', gameOne);
 
-var hats = prompt('how many hats does brian have?');
-console.log('num of hats:', hats);
+if (gameOne === 'Y') {
+  alert('Sweet!');
+} else {
+  alert('Not a problem.');
+} 
 
-if (kids === '3' || kids === '4') {
-  alert('u right');
+var gameTwo = prompt('do you play yugioh? Y or N only.');
+console.log('gameTwo:', gameTwo);
+
+if (gameTwo === 'Y') {
+  alert('Nice!');
+} else {
+  alert('Oh well.');
 }
 
-if (kids === '3' && hats ==='0') {
-  alert('u right again.');
+var gameThree = prompt('do you play pokemon? Y or N only.');
+console.log('gameThree:', gameThree);
+
+if (gameThree === 'Y') {
+  alert('Great!');
+} else {
+  alert('ok.')
+}
+
+var gameFour = prompt('do you play magic? Y or N only.');
+console.log('gameFour:', gameFour);
+
+if (gameFour === 'Y') {
+  alert('Awesome!');
+} else {
+  alert('Meh. Whatever.');
+}
+
+var gameFive = prompt('do you play buddyfight? Y or N only.');
+console.log('gameFive:', gameFive);
+
+if (gameFive === 'Y') {
+  alert('good!');
+} else {
+  alert('K then.');
 }
